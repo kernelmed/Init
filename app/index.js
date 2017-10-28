45103c3ebe28a14aa2c430153ec998400b9085f7
Mohammed Ben Said <bensaidmaroc@gmail.com>
2017-10-28 18:41:24 +0200

    First
    First

---
+++ app/index.js
@@ -0,0 +1,32 @@
+/**
+ * Auth0Sample 01-Custom-Form
+ * https://github.com/auth0/react-native-auth0
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import { StackNavigator } from 'react-navigation';
+
+const Navigation = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen }
+});
+
+export default class Auth0Sample extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Navigation />
+    );
+  }
+}
+
+AppRegistry.registerComponent('Auth0Sample', () => Auth0Sample);
